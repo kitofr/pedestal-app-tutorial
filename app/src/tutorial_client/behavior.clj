65f5f44250f4cd5a3ee8eff012a375de2b85c134
Kristoffer Roupé <kitofr@gmail.com>
2013-10-03 20:05:51 +0200

    Initial

---
+++ app/src/tutorial_client/behavior.clj
@@ -0,0 +1,25 @@
+(ns ^:shared tutorial-client.behavior
+    (:require [clojure.string :as string]
+              [io.pedestal.app :as app]
+              [io.pedestal.app.messages :as msg]))
+
+(defn set-value-transform [old-value message]
+  (:value message))
+
+(defn inc-transform [old-value _]
+  ((fnil inc 0) old-value))
+
+(defn init-main [_]
+  [[:transform-enable [:main :my-counter] :inc [{msg/topic [:my-counter]}]]])
+
+(defn swap-transform  [_ message]
+  (:value message))
+
+(def example-app
+  {:version 2
+   :transform [[:inc [:my-counter] inc-transform]
+               [:swap [:**]        swap-transform]]
+   :emit [{:init init-main}
+          [#{[:my-counter] [:other-counters :*]} (app/default-emitter [:main])]]})
+
+
