228af5a56e89175b2a1042f5c25c3c55f551bd9b
Kristoffer Roupé <kitofr@gmail.com>
2013-10-03 20:37:14 +0200

    Messages goes back to the server, average max total etc

--- app/src/tutorial_client/behavior.clj
+++ app/src/tutorial_client/behavior.clj
@@ -18,12 +18,34 @@
 (defn publish-counter [count]
   [{msg/type :swap msg/topic [:other-counters] :value count}])
 
+(defn total-count [_ nums] (apply + nums))
+
+(defn maximum [old-value nums]
+  (apply max (or old-value 0) nums))
+
+(defn average-count [_ {:keys [total nums]}]
+  (/ total (count nums)))
+
+(defn merge-counters [_ {:keys [me others]}]
+  (assoc others "Me" me))
+
 (def example-app
   {:version 2
    :transform [[:inc [:my-counter] inc-transform]
                [:swap [:**]        swap-transform]]
    :effect #{[#{[:my-counter]} publish-counter :single-val]}
+   :derive #{[#{[:my-counter] [:other-counters :*]} [:total-count] total-count :vals]
+             [#{[:my-counter] [:other-counters :*]} [:max-count] maximum :vals]
+             [{[:my-counter] :me [:other-counters] :others} [:counters] merge-counters :map]
+             [{[:my-counter] :nums [:other-counters :*] :nums [:total-count] :total} [:average-count] average-count :map]
+             }
    :emit [{:init init-main}
-          [#{[:my-counter] [:other-counters :*]} (app/default-emitter [:main])]]})
+          [#{[:my-counter] 
+             [:other-counters :*]
+             [:total-count]
+             [:max-count]
+             [:average-count]
+             } 
+           (app/default-emitter [:main])]]})
 
 
