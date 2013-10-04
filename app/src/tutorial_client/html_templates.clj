79f664471bc92674d34ac16234fff5df483619c5
Kristoffer Roupé <kitofr@gmail.com>
2013-10-04 21:15:19 +0200

    Moved .git to one folder up

--- app/src/tutorial_client/html_templates.clj
+++
@@ -1,6 +0,0 @@
-(ns tutorial-client.html-templates
-  (:use [io.pedestal.app.templates :only [tfn dtfn tnodes]]))
-
-(defmacro tutorial-client-templates
-  []
-  {:tutorial-client-page (dtfn (tnodes "tutorial-client.html" "hello") #{:id})})
